e858078ca990514e6564b5fc8506329f1dbf4fa3
Charles Wimmer <charles@wimmer.net>
2012-03-03 12:23:07 -0800

    Initial revision.

---
+++ lib/WWW/YQL.pm
@@ -0,0 +1,47 @@
+use strict;
+package WWW::YQL;
+use warnings;
+use Moose;
+# ABSTRACT: Module for YQL queries
+
+=head1 SYNOPSIS
+
+  None yet
+
+=head2 DESCRIPTION
+
+None yet
+
+=cut
+
+=method test
+
+No docs yet
+
+=cut
+
+sub test{
+    return;
+}
+1;
+=head1 KNOWN BUGS
+
+None known at this time.  Please log issues at: 
+
+https://github.com/cwimmer/www-yql/issues
+
+=head1 AVAILABILITY
+
+Source code is available on GitHub:
+
+https://github.com/cwimmer/www-yql
+
+Module available on CPAN as WWW::YQL:
+
+http://search.cpan.org/~cwimmer/
+
+=cut
+
+=begin Pod::Coverage
+
+=end Pod::Coverage
